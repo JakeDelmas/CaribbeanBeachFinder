1164ed2e8417c946d976dbb3d508e28a8798f724
OneDamien <damien.r@live.com>
2016-08-28 10:48:18 -0300

    index.php: Passed country to beach_info.php page. Tested that this info can be displayed

---
+++ pages/beach_info.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width,initial-scale=1">
+		<title>Caribbean Beach Finder</title>
+	  <script type="text/javascript">
+	   var _gaq = _gaq || [];
+	   _gaq.push(['_setAccount', 'UA-XXXXXXXX-Y']);
+	   _gaq.push(['_trackPageview']);
+	   (function()
+	   {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	   })();
+	  </script>
+
+	  <link rel="stylesheet" type="text/css" href="css/master.css">
+	  <?php include '../utilities/db_connect.php';?>
+  </head>
+	<body>
+	<?php
+	    echo $_POST['dd-menu-country'];
+	    //$sql = "SELECT BEACHES FROM " . $var_value .;
+        //$result = $conn->query($sql);
+
+    ?>
+	</body>
+</html>
