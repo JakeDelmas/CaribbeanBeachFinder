26ab7f6a1014e667e0fc9e69e067c0ca5e0195eb
mrfevrier <cp.fevr@gmail.com>
2016-09-01 20:22:50 -0300

    Added bootstrap CDN headers to header.php; added bootstrap container and rows to index.php;

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Caribbean Beach Finder</title>
         <script type="text/javascript">
             var _gaq = _gaq || [];
@@ -15,8 +15,25 @@
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
             })();
         </script>
+
+        <!--    Bootstrap   -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"> 
+
+        <!-- Custom Style Sheet (Ours) -->
         <link rel="stylesheet" type="text/css" href="css/master.css">
 		<script type="text/javascript" src="../js/slider.js"></script>
+
+        <!-- JQuery minified-->
+        <script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+
+        <!--Font Awesome Icons CDN  -->
+        <script src="https://use.fontawesome.com/bf1f685128.js"></script>
     </head>
+<body>
+<div class="container-fluid">
 
     
--- index.php
+++ index.php
@@ -2,28 +2,39 @@
 require("utilities/db_connect.php");
 require("inc/header.php"); 
 ?>
-<body id="homePage">
-    <img src="img/beachfinder.gif" alt="Smiley face">
-    <!--Populate drop-down menu with country choices-->
-    <div id="beachDropDown">
-        <form action="./pages/beach_info.php" method="post">
-            <select name="dd-menu-country">
-                <option>Select Country</option>
-                <?php
-$sql    = "SELECT ID, NAME FROM COUNTRIES";
-$result = $conn->query($sql);
+<div id="homePage">
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "<option value=\"" . $row["ID"] . "\">" . $row["NAME"] . "</option>\r\n";
-    }
-} else {
-    echo "0 results";
-}
-?>
+    <div class="row">
+        <div class="col-sm-12">
+            <img src="img/beachfinder.gif" alt="Smiley face">
+        </div>
+    </div>
+    
+    <!--Populate drop-down menu with country choices-->
+    <div class="row row-centered">
+        <div class="col-sm-12 col-centered" id="beachDropDown">
+
+            <form action="./pages/beach_info.php" method="post">
+                <select name="dd-menu-country">
+                    <option>Select Country</option>
+                    <?php
+                        $sql    = "SELECT ID, NAME FROM COUNTRIES";
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<option value=\"" . $row["ID"] . "\">" . $row["NAME"] . "</option>\r\n";
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                    ?>
                 </select>
                 <input type="submit" value="Go">
             </form>
+
         </div>
+    </div>
+</div>
 <?php require("inc/footer.php"); ?>
