9f5cda8fe11afb8e5ca8e2431b8c6a6ef4537863
mrfevrier <cp.fevr@gmail.com>
2016-09-08 03:31:22 -0300

    Implemented bootstrap slider and formatted beach_info to list beaches available in selected country in the nav bar; also display preview of each beach in slider; not fully functional this upload is to give idea of where i am going with this design-wise

--- beach_info.php
+++ beach_info.php
@@ -27,16 +27,16 @@ require("inc/header.php");
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="index.php">Caribbean Beach Finder: <?php echo $current_country; ?> |</a>
+              <a class="navbar-brand" href="index.php">Caribbean Beach Finder: &nbsp;<small><?php echo $current_country; ?></small> |</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
                 <!-- Social Menu Links -->
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram fa-lg"></i></a></li>
               </ul>
 
               <ul class="nav navbar-nav navbar-right">
@@ -65,67 +65,130 @@ require("inc/header.php");
     </div>
 
 
+<div class="row">
+
+    <div class="col-lg-2 beach-name-nav">
+
+      <ul class="nav nav-pills nav-stacked">
+          <li role="presentation" class="active"><a href="#">Beaches in <?php echo $current_country; ?>&nbsp;:</a></li>
+    
+
+    <?php
+    // Query to load all beaches in country into sidebar
+
+
+      $country_id = $_POST['dd-menu-country'];
+      $sql        = "SELECT BEACH_NAME, ID FROM BEACHES WHERE COUNTRY_ID = \"" . $country_id . "\"";
+      $result     = $conn->query($sql);
+      if ($result) {
+        while ($row = $result->fetch_assoc()) {
+          echo "<li role=\"presentation\" data-id=\"". $row["ID"] ."\"><a href=\"#\">". $row["BEACH_NAME"] ."</a></li>";
+        }
+      }
+    ?>
+      </ul>
+    </div>
+
+
+    <!--  Bootstrap Carousel  -->
+
+    <div class="col-sm-12 col-lg-6">
+
+      <div id="my-slider" class="carousel slide" data-ride="carousel">
+
+        <!--  Indicators  dot nav   -->
+        <ol class="carousel-indicators">
+          <li data-target="#my-slider" data-slide-to="0" class="active"></li>
+          <li data-target="#my-slider" data-slide-to="1"></li>
+          <li data-target="#my-slider" data-slide-to="2"></li>
+        </ol>
+
+         <!--  Wrapper to hold all slides  -->
+         <div class="carousel-inner" role="listbox">
+
+
+        <?php
+          $active_flag = 1;
+          $country_id = $_POST['dd-menu-country'];
+          $sql2        = "SELECT BEACH_NAME, ID FROM BEACHES WHERE COUNTRY_ID = \"" . $country_id . "\"";
+          $result2     = $conn->query($sql2);
+          if ($result2) {
+
+            while ($row2 = $result2->fetch_assoc()) {
+
+              // Now get file name for images of specific beaches
+
+              $sql3 = "SELECT FILE_NAME FROM IMAGES WHERE BEACH_ID = \"" . $row2["ID"] . "\"";
+              $result3 = $conn->query($sql3);
+              if ($result3) {
+                
+                //load first image per beach ONLY into Carousel
+                $row3 = $result3->fetch_assoc();
+                if (!$row3["FILE_NAME"]) {
+                  //If no images, break
+                  break;
+                }
+                
+                //<!--  Each slide is represented by one of these divs  -->
+
+                //Set active class only to first slide
+                if ($active_flag) {
+                  echo "<div class=\"item active\">";
+                }else{
+                  echo "<div class=\"item\">";
+                }
+
+                echo "
+                  <img src=\"" . $row3["FILE_NAME"] . "\" alt=\"beach1\">
+                  <div class=\"carousel-caption\">
+                    <h3>" . $row2["BEACH_NAME"] . "</h3>
+                    <p>About Beach 1</p>
+                  </div>
+                </div>";
+
+                //Stop applying active class to Carousel items
+                $active_flag = 0; 
+                
+              }
+            }
+          }
+
+        ?>
+        </div> <!-- Ends carousel-inner -->
+        
+        
+
+
+        
+
+        <!--  Controls next and prev  -->
+        <a class="left carousel-control" href="#my-slider" role="button" data-slide="prev">
+          <span class="icon-prev" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
+
+        <a class="right carousel-control" href="#my-slider" role="button" data-slide="next">
+          <span class="icon-next" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+
+      </div>
+
+    </div>
+
+    <!-- End Bootstrap Carousel -->
+
+    <div class="row" id="additionalInformation" style="display: none; ">
+
+    </div>
+    
+</div>
 
 
 	
-<div class="row" id="sliders">
-    <?php
-        $country_id = $_POST['dd-menu-country'];
-        $sql        = "SELECT BEACH_NAME, ID FROM BEACHES WHERE COUNTRY_ID = \"" . $country_id . "\"";
-        $result     = $conn->query($sql);
-        if ($result->num_rows > 0) {
-        echo "<div class=\"col-sm-12 webpage-container\">";
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-                //echo $row["BEACH_NAME"];
-                $sql         = "SELECT FILE_NAME FROM IMAGES WHERE BEACH_ID = \"" . $row["ID"] . "\"";
-                $result2     = $conn->query($sql);
-                $num_of_rows = ($result2->num_rows);
-                if ($result2->num_rows > 0) {
 
-
-                    echo "<div class=\"slideshow-container\">";
-					echo "<center><h1 id=\"beachTitle\">".$row["BEACH_NAME"]."</h1></center>";
-					echo "<button type=\"button\" class=\"btn btn-info\" onclick=\"displayAddInfo(".$row["ID"].",'".$row["BEACH_NAME"]."')\">Additional Info</button>";
-                    while ($row1 = $result2->fetch_assoc()) {
-                        //echo "<img src=\"".$row1["FILE_NAME"]."\" style=\"width:100%;\" alt=\"".$row["BEACH_NAME"]."\">";
-
-
-                        echo "<div class=\"" . $row["BEACH_NAME"] . " customfade\">";
-                        echo "<img src=\"" . $row1["FILE_NAME"] . "\" alt=\"" . $row["BEACH_NAME"] . "\">";
-                        //echo "<div class=\"text\">" . $row["BEACH_NAME"] . "</div>
-                                    echo "</div>";
-
-
-                    }
-                    echo "<a class=\"prev\" onclick=\"plusSlides(-1, '" . $row["BEACH_NAME"] . "')\">&#10094;</a>
-                                    <a class=\"next\" onclick=\"plusSlides(1, '" . $row["BEACH_NAME"] . "')\">&#10095;</a>
-
-                                </div>
-                            
-
-                                <div class=\"row clear text-center\">
-                                    <div class=\"col-sm-2 col-centered dot-container\">";
-                    for ($dots = 1; $dots <= $num_of_rows; $dots++) {
-                        echo "<span class=\"dot\" onclick=\"currentSlide(" . $dots . ", '" . $row["BEACH_NAME"] . "')\"></span> ";
-                    }
-
-                    echo "</div>
-                            </div>";
-
-
-                    echo "<br>";
-
-                    echo "<script type=\"text/javascript\"> makeSlider(\"" . $row["BEACH_NAME"] . "\")</script>";
-                }
-            }
-            echo "</div>";
-        }
-        ?>
-</div>
-  <div class="row" id="additionalInformation" style="display: none; ">
-
-  </div>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
